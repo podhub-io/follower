8a3fb9b85446b13d469e48f6d13502d2a7f25f9d
Jon Chen <bsd@voltaire.sh>
2015-04-05 15:08:58 -0700

    configure app

--- follower/__init__.py
+++ follower/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from werkzeug.exceptions import default_exceptions
 from werkzeug.exceptions import HTTPException
+import os
 import pylibmc
 
 
@@ -13,7 +14,16 @@ def make_json_error(ex):
     return response
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.update(
+    MEMCACHED_HOST='127.0.0.1',
+    URL_PARSE_TIMEOUT=86400,  # 1 day
+)
+
+if os.access(os.path.expanduser('~/.config/podhub/follower/config.py')):
+    app.config.from_pyffile('~/.config/podhub/follower/config.py', silent=True)
+elif os.access('/etc/podhub/follower/config.py'):
+    app.config.from_pyffile('/etc/podhub/follower/config.py', silent=True)
+
 mc = pylibmc.Client(app.config.get('MEMCACHED_HOST', '127.0.0.1'),
                     binary=True,
                     behaviors={'tcp_nodelay': True, 'ketama': True})
