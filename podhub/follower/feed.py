3ee9d4707187f8721d865faf44cc3bef14e12796
Jon Chen <bsd@voltaire.sh>
2015-04-07 01:01:28 -0700

    add logging

--- podhub/follower/feed.py
+++ podhub/follower/feed.py
@@ -1,6 +1,7 @@
 from . import app, mc
 from hashlib import md5
 import feedparser
+import json
 import uuid
 
 
@@ -17,16 +18,25 @@ class Feed(object):
 
     def __init__(self, url=None, **kwargs):
         self.url = url
+        app.logger.debug(json.dumps({'url': self.url}))
         if url is not None:
             self.parse_feed()
         self.lookup = {entry.entry_id: entry for entry in self.entries}
 
     def parse_feed(self):
         url_hash = md5(self.url).hexdigest()
+        app.logger.info(json.dumps({'url': self.url, 'url_hash': url_hash}))
 
         d = mc.get(url_hash)
-        if not d:
+        if d:
+            app.logger.debug(
+                json.dumps({'msg': 'Feed title {} parsed.'.format(d.title),
+                            'from_cache': True}))
+        else:
             d = feedparser.parse(self.url)
+            app.logger.debug(
+                json.dumps({'msg': 'Feed title {} parsed.'.format(d.title),
+                            'from_cache': False}))
             mc.set('/parsed_url/{}'.format(url_hash), d,
                    time=app.config.get('URL_PARSE_TIMEOUT'))
 
@@ -36,12 +46,19 @@ class Feed(object):
         self._custom_keys = feed_obj
 
         _feed_hash = md5(''.join([self.url, self.title])).hexdigest()
+        app.logger.info(json.dumps({'feed_hash': _feed_hash}))
         self.feed_id = mc.get('/feeds/{}'.format(_feed_hash))
-        if not self.feed_id:
+        if self.feed_id:
+            app.logger.info(json.dumps({'feed_id': self.feed_id,
+                                        'from_cache': True}))
+        else:
             self.feed_id = uuid.uuid4()
+            app.logger.info(json.dumps({'feed_id': self.feed_id,
+                                        'from_cache': False}))
             mc.set('/feeds/{}'.format(_feed_hash), self.feed_id)
 
         self.entries = tuple(Entry(**dict({'feed_id': self.feed_id}, **entry)) for entry in d.entries)  # noqa
+        app.logger.debug(json.dumps({'entries_in_feed': len(self.entries)}))
 
 
 
@@ -70,10 +87,14 @@ class Entry(object):
         mc_path = '/audio/{}/{}'.format(self.feed_id, self.entry_id)
         audio_link = mc.get(mc_path)
         if audio_link:
+            app.logger.debug(json.dumps({'audio_link': audio_link,
+                                         'from_cache': True}))
             return audio_link
         else:
             audio_link = filter(
                 lambda x: x.get('rel') == 'enclosure', self.links)[0].get(
                     'href')
             mc.set(mc_path, audio_link)
+            app.logger.debug(json.dumps({'audio_link': audio_link,
+                                         'from_cache': False}))
             return audio_link
--- podhub/follower/views.py
+++ podhub/follower/views.py
@@ -1,3 +1,4 @@
+import json
 from . import app
 from feed import Feed
 from flask import jsonify, request
@@ -19,12 +20,19 @@ def feed():
         index = -1
 
     feed = Feed(url=url)
+    app.logger.debug(json.dumps({'feed_title': feed.title}))
 
     try:
         entry = feed.entries[index]
+        app.logger.debug(json.dumps({'entry_title': entry.title}))
     except TypeError:
-        return jsonify(error_message='index must be an integer.'), 400
+        err = 'index must be an integer.'
+        app.logger.info(json.dumps({'user_error': err}))
+        return jsonify(error_message=err), 400
     except IndexError:
-        return jsonify(error_message='episode {} not found'.format(index)), 400
+        err = 'episode {} not found'.format(index)
+        app.logger.info(json.dumps({'error': err}))
+        return jsonify(error_message=), 404
 
-    return jsonify(feed_url=entry.audio)
+    app.logger.debug(json.dumps({'audio_url': entry.audio}))
+    return jsonify(audio_url=entry.audio)
