a5c9cf2016928781a591b2cefcb278b4b8c221c2
Jon Chen <bsd@voltaire.sh>
2015-04-13 00:59:36 -0700

    0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='podhub.follower',
-    version='v0.0.3',
+    version='0.0.4',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['podhub'],
     include_package_data=True,
